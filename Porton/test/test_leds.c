89751dccaa9646bc025b4675b799b6c5c3c5681e
Alejandro Gorrin <alejandro.gorrin.24@gmail.com>
2020-04-22 17:59:11 -0300

    test creados

---
+++ Porton/test/test_leds.c
@@ -0,0 +1,14 @@
+
+#include "unity.h"
+#include "leds.h"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+//Despues de la inicializacion, todos los leds deben qedar apagados
+void test_LedsOffAfterCreate(void){                 //Primer prueba
+	TEST_FAIL_MESSAGE("Empezamos");
+}
---
+++ Porton/test/test_porton.c
@@ -0,0 +1,13 @@
+#include "unity.h"
+#include "porton.h"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+//Despues de la inicializacion, todos los leds deben qedar apagados
+void test_LedsOffAfterCreate(void){                 //Primer prueba
+	TEST_FAIL_MESSAGE("Empezamos");
+}
